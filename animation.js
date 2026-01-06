5e6bf3b9ffb3cebb49676cc8b8d558e736c8ee77
renx-hub <whosback31@gmail.com>
2026-01-07 00:16:59 +0800

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
     $(".nav-buttons").hover(function(){
-        $(this).css("color", "rgba(255, 157, 46, 1)")
+        $(this).css("color", "rgba(192, 102, 0, 1)")
     });
 
     $("#topBar").mouseenter(function(){
-        $(this).css("background-color", "hsla(69, 100%, 86%, 1.00)")
+        $(this).css("background-color", "hsla(0, 0%, 100%, 1.00)")
+        $("#image").css("opacity", "1")
+        $("#name").css("opacity", "1")
     });
-    
+
     $("#topBar").mouseleave(function(){
-        $(this).css("background-color", "hsla(69, 100%, 86%, 0.56)")
+        $(this).css("background-color", "hsla(0, 0%, 100%, 0.27)")
+        $("#image").css("opacity", "0.5")
+        $("#name").css("opacity", "0.5")
     });
     
 });
